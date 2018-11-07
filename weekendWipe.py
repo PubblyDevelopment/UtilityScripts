24ee3c489e98c364b5fa2d163f60629542e5f6be
System Administrator <root@CCIs-Mac-mini-2.local>
2018-11-07 16:55:48 -0500

    Added weekendWipe file, it clears selected tables and files in a console type structure

---
+++ weekendWipe.py
@@ -0,0 +1,25 @@
+import MySQLdb
+import sys, os
+import shutil
+
+###### Empty out MySQL tables ######
+
+tables = ['map','books','childassetnotes','levels','schools','subjects','swaps','unitpages','units'] 
+
+db = MySQLdb.connect(host='localhost', db='console', user='console', password='somePassword')
+
+cur = db.cursor()
+
+for t in tables:
+	cur.execute("DELETE FROM %s WHERE 1;" % t)
+
+db.commit()
+
+db.close()
+
+###### Remove contents of appropriate directories ######
+
+folders = ['books','zips','schools','deletedBooks','units','deletedChildren','series','deletedSeries','map']
+
+for f in folders:
+	os.system("rm -r -f /var/www/html/%s/*" % f)
